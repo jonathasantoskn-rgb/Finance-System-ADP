317c67aa82aa515dad1fd9f10f373601ac953b9b
jonathasantoskn-rgb <jonatha.santos.kn@gmail.com>
2026-06-12 00:34:19 -0300

    Streamlit_app_ADP.py

--- Streamlit_app_ADP.py
+++ Streamlit_app_ADP.py
@@ -30,7 +30,7 @@ def modulo_entrada():
             with st.spinner("IA analisando documento..."):
                 try:
                     # Modelo mais estável e capaz
-                    model = genai.GenerativeModel('gemini-1.5-flash')
+                    model = genai.GenerativeModel('gemini-1.5-pro')
                     bytes_data = arquivo.getvalue()
                     
                     # Define o tipo MIME correto
